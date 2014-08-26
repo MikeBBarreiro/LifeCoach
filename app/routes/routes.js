2ea8afa2273c9505c8878098bc5e25dd084091bb
Michael Barreiro <Mikeb_barreiro@yahoo.com>
2014-08-26 12:13:04 -0500

    initial files

---
+++ app/routes/routes.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var morgan         = require('morgan'),
+    bodyParser     = require('body-parser'),
+    methodOverride = require('express-method-override'),
+    less           = require('less-middleware'),
+    session        = require('express-session'),
+    RedisStore     = require('connect-redis')(session),
+    security       = require('../lib/security'),
+    debug          = require('../lib/debug'),
+    home           = require('../controllers/home'),
+    goals          = require('../controllers/goals'),
+    users          = require('../controllers/users');
+
+module.exports = function(app, express){
+  app.use(morgan('dev'));
+  app.use(less(__dirname + '/../static'));
+  app.use(express.static(__dirname + '/../static'));
+  app.use(bodyParser.urlencoded({extended:true}));
+  app.use(methodOverride());
+  app.use(session({store:new RedisStore(), secret:'my super secret key', resave:true, saveUninitialized:true, cookie:{maxAge:null}}));
+
+  app.use(security.authenticate);
+  app.use(debug.info);
+
+  app.get('/', home.index);
+  app.get('/register', users.new);
+  app.post('/register', users.create);
+  app.get('/login', users.login);
+  app.post('/login', users.authenticate);
+
+  app.use(security.bounce);
+  app.delete('/logout', users.logout);
+  app.get('/goals/new', goals.new);
+  app.post('/goals', goals.create);
+
+  console.log('Express: Routes Loaded');
+};
+
